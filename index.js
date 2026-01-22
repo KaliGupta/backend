992fa32ace96a4a7d3cc33b2680204158494f46d
KaliGupta <guptakali480@gmail.com>
2026-01-22 14:00:56 +0530

    github data added

--- index.js
+++ index.js
@@ -3,6 +3,45 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const githubData = {
+  "login": "KaliGupta",
+  "id": 130894573,
+  "node_id": "U_kgDOB81K7Q",
+  "avatar_url": "https://avatars.githubusercontent.com/u/130894573?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/KaliGupta",
+  "html_url": "https://github.com/KaliGupta",
+  "followers_url": "https://api.github.com/users/KaliGupta/followers",
+  "following_url": "https://api.github.com/users/KaliGupta/following{/other_user}",
+  "gists_url": "https://api.github.com/users/KaliGupta/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/KaliGupta/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/KaliGupta/subscriptions",
+  "organizations_url": "https://api.github.com/users/KaliGupta/orgs",
+  "repos_url": "https://api.github.com/users/KaliGupta/repos",
+  "events_url": "https://api.github.com/users/KaliGupta/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/KaliGupta/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Kali Gupta",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": "Final year Btech student || Front-End Developer\r\n",
+  "twitter_username": null,
+  "public_repos": 3,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 1,
+  "created_at": "2023-04-16T07:58:38Z",
+  "updated_at": "2025-07-29T12:11:36Z"
+}
+
+app.get('/github', (req,res)=>{
+  res.json(githubData)
+})
 app.get('/',(req,res)=>{
   res.send("hii")
 })
@@ -18,3 +57,4 @@ app.get('/signup',(req,res)=>{
 app.listen(process.env.PORT,()=>{
     console.log(`app is running on ${port}`)
 })
+
